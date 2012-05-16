41d03080c4de4dd032b7559384b91a1a7d1408d6
Andrew Zheng <zhengman777@gmail.com>
2012-05-15 20:16:48 -0400

    Added variables and methods to make the code more intuitive. Changed constructor.

--- src/catt/kedavra/characters/Player.java
+++ src/catt/kedavra/characters/Player.java
@@ -1,7 +1,6 @@
 package catt.kedavra.characters;
 
 import java.util.ArrayList;
-import org.newdawn.slick.geom.Vector2f;
 import catt.kedavra.components.Component;
 import catt.kedavra.entities.Collidable;
 import catt.kedavra.entities.Entity;
@@ -9,15 +8,46 @@ import catt.kedavra.entities.Entity;
 
 public class Player extends Entity {
 	
-	int id;
-	int collisionType;
+	private int id;
+	private int collisionType;
+	private float baseSpeed = .2f;
+	//Speed caps
+	private float maxWalk = .3f;
+	private float maxSprint = .7f;
+	//Acceleration values
+	private float walkAccel;
+	private float sprintAccel;
 	ArrayList<Component> components;
-	public Vector2f position;
 	
-	public Player(int id, int collisionType) {
+	public Player(int id, int collisionType, float baseSpeed, float maxWalk, float maxSprint, float walkAccel, float sprintAccel) {
 		super(id, collisionType);
 		this.id = id;
 		this.collisionType = collisionType;
+		this.baseSpeed = baseSpeed;
+		this.maxWalk = maxWalk;
+		this.maxSprint = maxSprint;
+		this.walkAccel = walkAccel;
+		this.sprintAccel = sprintAccel;
+	}
+	
+	public float getBaseSpeed() {
+		return baseSpeed;
+	}
+	
+	public float getMaxWalk() {
+		return maxWalk;
+	}
+	
+	public float getWalkAccel() {
+		return walkAccel;
+	}
+	
+	public float getMaxSprint() {
+		return maxSprint;
+	}
+	
+	public float getSprintAccel() {
+		return sprintAccel;
 	}
 	
 	public void collision(Collidable other) {
